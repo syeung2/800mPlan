e2be4f33c4159e59f666b09ae5c3a0d6fd4b0e84
syeung2 <syeung2@users.noreply.github.com>
2023-01-04 20:10:31 +0000

    deploy: f073c138be2d993878fd961af1bc571a635918ca

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/xc22/assets/js/search-data.json', true);
+    request.open('GET', '/800mPlan/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
